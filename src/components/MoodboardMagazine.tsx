3d68f0aef43204b9d4f8efbacf570589964b5644
李佳馨 <lijiaxin@lijiaxindeMacBook-Pro.local>
2026-04-13 03:59:28 +0800

    Fix moodboard white space with fixed-row collage layout

--- src/components/MoodboardMagazine.tsx
+++ src/components/MoodboardMagazine.tsx
@@ -20,46 +20,30 @@ export default function MoodboardMagazine({
 
       {/* Asymmetric mosaic: left 1/3 tall, right 2/3 staggered cards (tight gaps) */}
       <div className="mt-6 overflow-hidden border border-black/10 bg-white">
-        <div className="grid gap-0 md:grid-cols-12">
-          <div className="md:col-span-4">
-            <div className="h-full">
-              <div className="aspect-[3/4]">
-                {/* eslint-disable-next-line @next/next/no-img-element */}
-                <img src={primary.src} alt={primary.alt} className="h-full w-full object-cover" />
-              </div>
-            </div>
+        <div className="grid gap-0 grid-cols-12 auto-rows-[120px] md:auto-rows-[160px]">
+          <div className="col-span-4 row-span-4">
+            {/* eslint-disable-next-line @next/next/no-img-element */}
+            <img src={primary.src} alt={primary.alt} className="h-full w-full object-cover" />
           </div>
 
-          <div className="md:col-span-8">
-            <div className="grid gap-0 md:grid-cols-2">
-              <div className="md:col-span-2">
-                <div className="aspect-[16/9]">
-                  {/* eslint-disable-next-line @next/next/no-img-element */}
-                  <img src={seed.src} alt={seed.alt} className="h-full w-full object-cover" />
-                </div>
-              </div>
+          <div className="col-span-8 row-span-2">
+            {/* eslint-disable-next-line @next/next/no-img-element */}
+            <img src={seed.src} alt={seed.alt} className="h-full w-full object-cover" />
+          </div>
 
-              <div>
-                <div className="aspect-[4/3]">
-                  {/* eslint-disable-next-line @next/next/no-img-element */}
-                  <img src={patterns[0]?.src} alt={patterns[0]?.alt ?? "Pattern 1"} className="h-full w-full object-cover" />
-                </div>
-              </div>
+          <div className="col-span-4 row-span-2">
+            {/* eslint-disable-next-line @next/next/no-img-element */}
+            <img src={patterns[0]?.src} alt={patterns[0]?.alt ?? "Pattern 1"} className="h-full w-full object-cover" />
+          </div>
 
-              <div className="md:row-span-2">
-                <div className="aspect-[4/3] md:aspect-[3/4]">
-                  {/* eslint-disable-next-line @next/next/no-img-element */}
-                  <img src={patterns[1]?.src} alt={patterns[1]?.alt ?? "Pattern 2"} className="h-full w-full object-cover" />
-                </div>
-              </div>
+          <div className="col-span-4 row-span-4">
+            {/* eslint-disable-next-line @next/next/no-img-element */}
+            <img src={patterns[1]?.src} alt={patterns[1]?.alt ?? "Pattern 2"} className="h-full w-full object-cover" />
+          </div>
 
-              <div>
-                <div className="aspect-[4/3]">
-                  {/* eslint-disable-next-line @next/next/no-img-element */}
-                  <img src={patterns[2]?.src} alt={patterns[2]?.alt ?? "Pattern 3"} className="h-full w-full object-cover" />
-                </div>
-              </div>
-            </div>
+          <div className="col-span-4 row-span-2">
+            {/* eslint-disable-next-line @next/next/no-img-element */}
+            <img src={patterns[2]?.src} alt={patterns[2]?.alt ?? "Pattern 3"} className="h-full w-full object-cover" />
           </div>
         </div>
       </div>
